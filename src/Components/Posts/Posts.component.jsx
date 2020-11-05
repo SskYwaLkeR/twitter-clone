5869015726559964e2c9eb5d29b78980c01c0fd9
sskywalker <hkrai39@gmail.com>
2020-11-05 18:13:50 +0530

    added footer component

---
+++ src/Components/Posts/Posts.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Posts extends React.Component{
+    render(){
+        return (
+            <div className="posts-wrapper">
+                <div className="profile"></div>
+                <div className="post-container">
+                <div className="post-header"></div>
+                <div className="post-body"></div>
+                <div className="post-footer"></div>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default Posts
