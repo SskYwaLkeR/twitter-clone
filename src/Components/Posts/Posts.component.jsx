39ec8545dfc95864e3431b6f29aa4ff9eb3acda9
sskywalker <hkrai39@gmail.com>
2020-11-05 18:20:27 +0530

    added basic markup for posts

--- src/Components/Posts/Posts.component.jsx
+++ src/Components/Posts/Posts.component.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 
-class Posts extends React.Component{
-    render(){
+class Posts extends React.Component {
+    render() {
         return (
             <div className="posts-wrapper">
                 <div className="profile"></div>
                 <div className="post-container">
-                <div className="post-header"></div>
-                <div className="post-body"></div>
-                <div className="post-footer"></div>
+                    <div className="post-header"></div>
+                    <div className="post-body"></div>
+                    <div className="post-footer"></div>
                 </div>
-
             </div>
         )
     }
