29fd40fb9584f44a07c3208371bcb01456456c52
sskywalker <hkrai39@gmail.com>
2020-11-05 18:27:49 +0530

    small change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import './App.css'
+import Header from './Components/Header/Header.component.jsx'
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    )
 }
 
-export default App;
+export default App
